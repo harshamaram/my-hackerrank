f8d556ab89652ad645c5a42d689799703b2c44d6
Harsha Sravs <mhvardhan@gmail.com>
2019-10-02 20:54:48 -0400

    update

--- src/main/java/com/myquest/hackerrank/strings/medium/Regex2DuplicateWords.java
+++ src/main/java/com/myquest/hackerrank/strings/medium/Regex2DuplicateWords.java
@@ -1,6 +1,5 @@
 package com.myquest.hackerrank.strings.medium;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,26 +8,20 @@ public class Regex2DuplicateWords {
     public static void main(String[] args) {
 
         String regex = "/* Write a RegEx matching repeated words here. */";
-        Pattern p = Pattern.compile(regex, /* Insert the correct Pattern flag here.*/);
-
-        Scanner in = new Scanner(System.in);
-        int numSentences = Integer.parseInt(in.nextLine());
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE); ///* Insert the correct Pattern flag here.*/
         
-        while (numSentences-- > 0) {
-            String input = in.nextLine();
-            
-            Matcher m = p.matcher(input);
-            
-            // Check for subsequences of input that match the compiled pattern
-            while (m.find()) {
-                input = input.replaceAll(/* The regex to replace */, /* The replacement. */);
-            }
-            
-            // Prints the modified sentence.
-            System.out.println(input);
+        String input = "one two One three two four four FIVE five six sIx seven eight Seven";
+        
+        Matcher m = p.matcher(input);
+        
+        // Check for subsequences of input that match the compiled pattern
+        while (m.find()) {
+            input = input.replaceAll("", ""); // /* The regex to replace */, /* The replacement. */
         }
         
-        in.close();
+        // Prints the modified sentence.
+        System.out.println(input);
     }
 }
+
 
